cc88ac9e8f031b0d9d2ae276db2ac9beed4de735
pravalik12 <pspreddy121@gmail.com>
2021-09-25 16:34:40 +0530

    Updated Message.

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringAzureDemoApplication {
 
 	@GetMapping("/message")
 	public String message(){
-		return "Congrats ! your application deployed successfully in Azure Platform. !";
+		return "Pravalik Says Hi..!!!";
 	}
 
 	public static void main(String[] args) {
